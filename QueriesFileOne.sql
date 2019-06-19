1b20e49e76e72e0fd822eb72da1b11067165dd15
Bobandeep Singh <bobandeepsingh@gmail.com>
2019-06-19 16:16:06 +0530

    Like-Escape query

--- QueriesFileOne.sql
+++ QueriesFileOne.sql
@@ -1,5 +1,6 @@
---Query: 1
---This query will give you all table spaces name, there full capacit and free space left in MBs.
+--#1
+-- Table space query
+-- This query will give you all table spaces name, there full capacit and free space left in MBs.
 SELECT b.tablespace_name, tbs_size Size_Mb, a.free_space Free_Mb
 FROM
   (SELECT tablespace_name, ROUND(SUM(bytes)/1024/1024 ,2) AS free_space
@@ -9,36 +10,16 @@ FROM
   FROM dba_data_files GROUP BY tablespace_name ) b
 WHERE a.tablespace_name(+)=b.tablespace_name;
 
----Fun with dates
-SELECT trunc(sysdate) - (to_number(to_char(sysdate,'DD')) - 1) FROM dual; ---first day 0f current month
-Select trunc((sysdate),'mm') from dual; ---first day 0f current month
-select last_day(sysdate) from dual;   ---last day of current month
-select trunc(last_day(add_months(sysdate,-1))+1) from dual;   ---first day 0f last month
-Select trunc(trunc(sysdate,'MM')-1,'MM')  from dual;          ---first day 0f last month
-select last_day(sysdate)+1 from dual;   ---first day of next month
-Select sysdate from dual;
-SELECT TO_CHAR(SYSDATE, 'MM-DD-YYYY HH24:MI:SS') "Current_Time" FROM DUAL;
-select to_char(sysdate-(4/24), 'DD/Mon/YYYY HH24:MI:SS') from dual;
+--#2
+-- Like-Escape query
+-- In this query I'm changing the BTN coulmn value and removing the BTN's trailing characters from underscore "_"
+-- The problem I faced was that even I added a condition to just select the BTNs which have underscore in them.
+-- But the query was also selecting the BTNs which does not have it. This was because underscore is a special character.
+-- So, to inform Oracle about this special character and treat it as simple character by using ESCAPE.
+Select BTN, substr(BTN,1, INSTR(BTN,'_')-1) Updated_BTN 
+from PMT
+Where BAC = 'KENAN' and BTN like '%\_%' escape '\' order by CREATE_TIME desc;
 
-SELECT TO_CHAR(SYSDATE, 'MMDDYYYY_HH24:MI:SS') "Current_Time" FROM DUAL;
-
-SELECT EXTRACT(MONTH FROM sysdate) from dual;
-
-Select SYSTIMESTAMP from dual;
-
---Examples of the to_date function might include:
-/*
-to_date('10-12-06','MM-DD-YY');
-to_date('jan 2007','MON YYYY');
-to_date('2007/05/31','YYYY/MM/DD');
-to_date('12-31-2007 12:15','MM-DD-YYYY HH:MI');
-to_date('2006,091,00:00:00' , 'YYYY,DDD,HH24:MI:SS');
-to_date('15-may-2006 06:00:01','dd-mon-yyyy hh24:mi:ss');
-to_date('022002','mmyyyy');
-to_date('12319999','MMDDYYYY');
-to_date(substr( collection_started,1,12),'DD-MON-YY HH24');
-to_date('2004/10/14 21', 'yyyy/mm/dd hh24');
-TO_DATE(First_Load_Time, 'yyyy-mm-dd/hh24:mi:ss')*24*60);
-*/
-alter SESSION set NLS_DATE_FORMAT = 'DD-MM-YYYY HH24:MI:SS';
-
+-- Any character can follow ESCAPE except percent (%) and underbar (_). 
+-- A wildcard character is treated as a literal if preceded by the escape character.
+-- This means we can use any character as escape character like here we took back slash \
